36908a81808cd94a59814db209923096feb941c8
suntabu <suntabu@163.com>
2019-08-26 16:59:41 +0800

    update

--- Assets/Effects/E3/PaintPen.shader
+++ Assets/Effects/E3/PaintPen.shader
@@ -1,21 +1,38 @@
-﻿Shader "Unlit/PaintePen"
+﻿Shader "Painter/Pen"
 {
 	Properties
 	{
-		_MainTex ("Texture", 2D) = "white" {}
+		_MainTex ("Main Texture", 2D) = "white" {}
+		_MaskTex ("Mask Texture", 2D) = "white" {}
+		_BrushTex ("Brush Texture", 2D) = "white" {}
+		_Color("Color",Color)=(1,1,1,1)
+		_Alpha("Alpha",Range(0,1))=1
+		_BlendRatio("_BlendRatio",Range(0,1))=0.5
+		[Enum(UnityEngine.Rendering.BlendMode)] _BlendSrc("Src Factor",float)=5
+		[Enum(UnityEngine.Rendering.BlendMode)] _BlendDst("Dst Factor",float)=10
+		[Enum(UnityEngine.Rendering.BlendMode)] _FactorA("Factor A",float)=1
+		[Enum(UnityEngine.Rendering.BlendMode)] _FactorB("Factor B",float)=10
+		[Enum(UnityEngine.Rendering.CullMode)] _CullMode("Cull Mode",float)=0
+		_TileX("Tile x",Range(0,100))=1
+		_TileY("Tile y",Range(0,100))=1
+		_rect("Rect", Vector) = (0,0,0,0)
+		_canvas("Ratio Y", Vector) = (0,0,0,0) 
+		
 	}
 	SubShader
 	{
-		Tags { "RenderType"="Opaque" }
+		Tags { "RenderType"="Transparent" "Queue"="Transparent" "IgnoreProjector"="True"}
 		LOD 100
-
+		Cull [_CullMode]
+		ZTest Always
+		ZWrite off
+		Blend [_BlendSrc] [_BlendDst] , [_FactorA] [_FactorB]
+	
 		Pass
 		{
 			CGPROGRAM
 			#pragma vertex vert
 			#pragma fragment frag
-			// make fog work
-			#pragma multi_compile_fog
 			
 			#include "UnityCG.cginc"
 
@@ -28,19 +45,83 @@
 			struct v2f
 			{
 				float2 uv : TEXCOORD0;
+				float2 uv1 : TEXCOORD1;
+				float2 uv2 : TEXCOORD2;
 				UNITY_FOG_COORDS(1)
 				float4 vertex : SV_POSITION;
 			};
 
 			sampler2D _MainTex;
 			float4 _MainTex_ST;
+			sampler2D _BrushTex;
+			float4 _BrushTex_ST;
+			sampler2D _MaskTex;
+			float4 _MaskTex_ST;
+			fixed4 _Color;
+			fixed _Alpha;
+			float _TileX;
+			float _TileY;
+			float4 _rect;
+			float4 _canvas;
+			float _BlendRatio;
 			
+			float random (float2 uv)
+            {
+                return frac(sin(dot(uv,float2(12.9898,78.233)))*43758.5453123);
+            }
+            
+            fixed4 texNoTileTech1(sampler2D tex, float2 uv) {
+                float2 iuv = floor(uv);
+                float2 fuv = frac(uv);
+            
+                // Generate per-tile transformation
+                float4 ofa = random(iuv + float2(0, 0));
+                float4 ofb = random(iuv + float2(1, 0));
+                float4 ofc = random(iuv + float2(0, 1));
+                float4 ofd = random(iuv + float2(1, 1));
+            
+                // Compute the correct derivatives
+                float2 dx = ddx(uv);
+                float2 dy = ddy(uv);
+            
+                // Mirror per-tile uvs
+                ofa.zw = sign(ofa.zw - 0.5);
+                ofb.zw = sign(ofb.zw - 0.5);
+                ofc.zw = sign(ofc.zw - 0.5);
+                ofd.zw = sign(ofd.zw - 0.5);
+            
+                float2 uva = uv * ofa.zw + ofa.xy, dxa = dx * ofa.zw, dya = dy * ofa.zw;
+                float2 uvb = uv * ofb.zw + ofb.xy, dxb = dx * ofb.zw, dyb = dy * ofb.zw;
+                float2 uvc = uv * ofc.zw + ofc.xy, dxc = dx * ofc.zw, dyc = dy * ofc.zw;
+                float2 uvd = uv * ofd.zw + ofd.xy, dxd = dx * ofd.zw, dyd = dy * ofd.zw;
+            
+                // Fetch and blend
+                float2 b = smoothstep(_BlendRatio, 1.0 - _BlendRatio, fuv);
+            
+                return lerp(lerp(tex2D(tex, uva, dxa, dya), tex2D(tex, uvb, dxb, dyb), b.x),
+                                lerp(tex2D(tex, uvc, dxc, dyc), tex2D(tex, uvd, dxd, dyd), b.x), b.y);
+            }
+            
+            float2 calcUV2(float4 canvas, float4 rect, float2 uv)
+            {
+                float x = uv.x * rect.z + rect.x ;//- rect.z * 0.5;
+                float y = ( uv.y * rect.w + canvas.w - rect.y - rect.w);
+                float tileX = canvas.z / _TileX;
+                float tileY = canvas.w / _TileY;
+            
+                float2 result = float2(x/tileX, y/tileY);
+                
+                return result;
+            }
+
 			v2f vert (appdata v)
 			{
 				v2f o;
 				o.vertex = UnityObjectToClipPos(v.vertex);
 				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
-				UNITY_TRANSFER_FOG(o,o.vertex);
+				o.uv1 = TRANSFORM_TEX(v.uv, _MaskTex);
+				
+				o.uv2 = calcUV2(_canvas,_rect,v.uv);
 				return o;
 			}
 			
@@ -48,9 +129,22 @@
 			{
 				// sample the texture
 				fixed4 col = tex2D(_MainTex, i.uv);
-				// apply fog
-				UNITY_APPLY_FOG(i.fogCoord, col);
-				return col;
+				
+				fixed4 brush;
+				if(_TileX == 1 && _TileY == 1)
+				{
+				    brush = tex2D(_BrushTex, i.uv2 );
+				}else
+				{
+				    brush = texNoTileTech1(_BrushTex, i.uv2 );
+				}
+
+				brush.a = col.a;
+
+				fixed maskA = tex2D(_MaskTex, i.uv1).a*_Alpha;
+				brush *= maskA;
+
+				return brush;
 			}
 			ENDCG
 		}
---
+++ Assets/Effects/E3/Painter.cs
@@ -0,0 +1,183 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Painter : MonoBehaviour
+{
+    public Texture2D TargetTexture;
+    public Texture2D PenTexture;
+    public float BrushScale = 1;
+    [Range(0.01f, 2f)] public float DrawLerpDamp = 0.02f;
+    
+    public Material PenMat, CanvasMat;
+
+    private RenderTexture mRenderTexture1,mRenderTexture2;
+    private int mCanvasWidth, mCanvasHeight;
+    private bool mIsDrawing,mIsMouseDown;
+    private Vector3 mPreMousePosition;
+    private Rect mUV = new Rect(0f, 0f, 1f, 1f);
+    private void Awake()
+    {
+        mCanvasWidth = TargetTexture.width;
+        mCanvasHeight = TargetTexture.height;
+        
+        mRenderTexture1 = RenderTexture.GetTemporary(TargetTexture.width, TargetTexture.height);
+        mRenderTexture2 = RenderTexture.GetTemporary(TargetTexture.width, TargetTexture.height);
+        
+        PenMat.SetFloat("_BlendSrc", (int) BlendMode.SrcAlpha);
+        PenMat.SetFloat("_BlendDst", (int) BlendMode.OneMinusSrcAlpha);
+        
+        PenMat.SetTexture("_BrushTex", TargetTexture);
+        
+        
+        CanvasMat.SetFloat("_BlendSrc", (int) BlendMode.One);
+        CanvasMat.SetFloat("_BlendDst", (int) BlendMode.OneMinusSrcAlpha);
+        
+        CanvasMat.SetTexture("_MainTex", mRenderTexture1);
+        
+        CreateQuad(CanvasMat);
+    }
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            mIsMouseDown = true;
+            //Draw once when mouse down.
+            //ClickDraw(Input.mousePosition,Camera.main,painterCanvas.penTex,painterCanvas.brushScale,painterCanvas.penMat,painterCanvas.renderTexture);
+        }
+        else if(Input.GetMouseButton(0)){
+            if(mIsMouseDown)
+            {
+                //draw on mouse drag.
+                Drawing(Input.mousePosition,Camera.main);
+            }
+        }
+        else if(Input.GetMouseButtonUp(0) && mIsMouseDown)
+        {
+            EndDraw();
+            mIsMouseDown = false;
+        }
+    }
+    
+    /// <summary>
+    /// Convert hit point to uv position.
+    /// </summary>
+    /// <returns>uv position.</returns>
+    /// <param name="hitPoint">Hit point is world position</param>
+    Vector2 SpriteHitPoint2UV(Vector3 hitPoint)
+    {
+        Vector3 localPos = transform.InverseTransformPoint(hitPoint);
+        localPos *= 100f;
+        localPos.x += mCanvasWidth * 0.5f;
+        localPos.y += mCanvasHeight * 0.5f;
+        return new Vector2(localPos.x / mCanvasWidth, localPos.y / mCanvasHeight);
+    }
+    
+    public void EndDraw()
+    {
+        mIsDrawing = false;
+    }
+    
+    public void Drawing(Vector3 screenPos, Camera camera = null, bool drawOutside = false)
+    {
+        if (camera == null) camera = Camera.main;
+        Vector3 uvPos = SpriteHitPoint2UV(camera.ScreenToWorldPoint(screenPos));
+        screenPos = new Vector3(uvPos.x * mCanvasWidth, mCanvasHeight - uvPos.y * mCanvasHeight, 0f);
+        if (!mIsDrawing)
+        {
+            mIsDrawing = true;
+            mPreMousePosition = screenPos;
+        }
+
+        if (mIsDrawing)
+        {
+            GL.PushMatrix();
+            GL.LoadPixelMatrix(0, mCanvasWidth, mCanvasHeight, 0);
+            RenderTexture.active = mRenderTexture1;
+  
+            LerpDraw(ref screenPos, ref mPreMousePosition, drawOutside);
+
+            RenderTexture.active = null;
+            GL.PopMatrix();
+            mPreMousePosition = screenPos;
+        }
+    }
+    
+    bool Intersect(ref Rect a, ref Rect b)
+    {
+        bool c1 = a.xMin < b.xMax;
+        bool c2 = a.xMax > b.xMin;
+        bool c3 = a.yMin < b.yMax;
+        bool c4 = a.yMax > b.yMin;
+        return c1 && c2 && c3 && c4;
+    }
+    
+    void CreateQuad(Material mat)
+    {
+        Mesh m = new Mesh();
+        m.vertices = new Vector3[]
+        {
+            new Vector3(mCanvasWidth * 0.005f, mCanvasHeight * 0.005f),
+            new Vector3(mCanvasWidth * 0.005f, -mCanvasHeight * 0.005f),
+            new Vector3(-mCanvasWidth * 0.005f, -mCanvasHeight * 0.005f),
+            new Vector3(-mCanvasWidth * 0.005f, mCanvasHeight * 0.005f)
+        };
+        m.uv = new Vector2[]
+        {
+            new Vector2(1, 1),
+            new Vector2(1, 0),
+            new Vector2(0, 0),
+            new Vector2(0, 1)
+        };
+        m.triangles = new int[] {0, 1, 2, 2, 3, 0};
+        m.RecalculateBounds();
+        m.RecalculateNormals();
+
+        MeshFilter meshFilter = gameObject.GetComponent<MeshFilter>();
+        if (meshFilter == null) meshFilter = gameObject.AddComponent<MeshFilter>();
+        meshFilter.mesh = m;
+
+        MeshRenderer rend = gameObject.GetComponent<MeshRenderer>();
+        if (rend == null) rend = gameObject.AddComponent<MeshRenderer>();
+        rend.material = mat;
+//        rend.sortingLayerName = sortingLayerName;
+//        rend.sortingOrder = sortingOrder;
+    }
+    void LerpDraw(ref Vector3 current, ref Vector3 prev, bool drawOutside)
+    {
+        float distance = Vector2.Distance(current, prev);
+        if (distance > 0f)
+        {
+            Vector2 pos;
+            float w = PenTexture.width * BrushScale;
+            float h = PenTexture.height * BrushScale;
+            float lerpDamp = Mathf.Min(w, h) * DrawLerpDamp;
+            mUV.width = mCanvasWidth;
+            mUV.height = mCanvasHeight;
+            for (float i = 0; i < distance; i += lerpDamp)
+            {
+                float lDelta = i / distance;
+                float lDifx = current.x - prev.x;
+                float lDify = current.y - prev.y;
+                pos.x = prev.x + (lDifx * lDelta);
+                pos.y = prev.y + (lDify * lDelta);
+                Rect rect = new Rect(pos.x - w * 0.5f, pos.y - h * 0.5f, w, h);
+                if (drawOutside || Intersect(ref mUV, ref rect))
+                {
+                    PenMat.SetVector("_rect", new Vector4(rect.x, rect.y, rect.width, rect.height));
+                    PenMat.SetVector("_canvas",
+                        new Vector4(-mCanvasWidth / 2, mCanvasHeight / 2 + transform.localPosition.y * 200,
+                            mUV.width, mUV.height));
+
+                    Debug.Log(PenMat.GetVector("_rect") + " -> " + PenMat.GetVector("_canvas"));
+                    Graphics.DrawTexture(rect, PenTexture, PenMat);
+                }
+            }
+        }
+    }
+}
