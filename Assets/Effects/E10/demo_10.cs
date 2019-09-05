a115f46bea9460076982398c64a34df45500d982
gouzhun <gouzhun@nexa-corp.com>
2019-09-05 21:04:31 +0800

    add content

---
+++ Assets/Effects/E10/demo_10.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class demo_10 : MonoBehaviour
+{
+    public Slider Slider;
+    void Start()
+    {
+        Slider.onValueChanged.AddListener(new UnityAction<float>(t =>
+        {
+            this.GetComponent<MeshRenderer>().material.SetFloat("_Progress",t);
+        }));
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
